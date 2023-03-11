1a6e3b972d0b3d097473910006615c0723d32cbc
ismail <iharmanda@hotmail.com>
2023-03-12 02:17:52 +0300

    add onPress action to roll button and change UI  via this IBAction

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,13 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2:UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
-        diceImageView1.alpha=0.5
-        
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+    }
 }
 
