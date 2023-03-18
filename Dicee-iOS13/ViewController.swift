d96fcea4ece03b4643384b66b16e3b571720b2e0
ismail <iharmanda@hotmail.com>
2023-03-18 15:40:35 +0300

    add randomness to roll button and make it with a better syntax

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -11,15 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
-    @IBOutlet weak var diceImageView2:UIImageView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+    @IBOutlet weak var diceImageView2: UIImageView!
+    let diceArray=[UIImage(imageLiteralResourceName: "DiceOne"),UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix")]
+
 
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
-        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image=diceArray.randomElement()
+        
     }
 }
 
